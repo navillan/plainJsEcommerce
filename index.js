6096325eeebaf3e8e2d7d571b5ed5ed5c0d6552b
Mert <espawnn@gmail.com>
2025-11-07 16:35:48 +0300

    Project v0.3

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ $.ajax({
   success: (data) => {
     const products = data.products;
     products.forEach((item) => {
-    $carouselMain.append(`<div class="item-card">
+    $carouselMain.append(`
+      <div class="item-card">
        <div class="item-rating">★${item.rating}</div>
        <div class="item-image" style="background-image: url('${item.thumbnail}')"></div>
        <div class="item-title">${item.title}</div>
        <div class="item-price">$${item.price}</div>
-      </div>`);
-  });
+      </div>
+      `);
+    });
+    products.slice(12, 16).forEach((item) => {
+      $mainFeaturedItems.append(`
+        <div class="featured-product-item-route">        
+          <div class="featured-product-image" style="background-image: url('${item.thumbnail}')"></div>
+          <div class="featured-product-title">${item.title}</div>
+          <div class="featured-product-description">${item.description}</div>
+        </div>
+      `)
+    })
   },
   error: (error) => {
     console.error("API error:", error);
@@ -50,3 +61,6 @@ function resetCarousel() {
   currentOffset = 0;
   $carouselMain.css('transform', `translateX(0)`);
 }
+
+//
+const $mainFeaturedItems = $('.main-featured-item-cards');
